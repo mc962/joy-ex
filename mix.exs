b7299516911b6735facfc00377befb16a146aae0
mc962 <11969409+mc962@users.noreply.github.com>
2026-03-27 20:47:54 -0400

    Implement optional replica database

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Joy.MixProject do
   def project do
     [
       app: :joy,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
