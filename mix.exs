cf3689c5b3440d432469fb6c929683abd0b1d5dd
mc962 <11969409+mc962@users.noreply.github.com>
2026-03-29 09:44:14 -0400

    Fix build issues

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Joy.MixProject do
   def project do
     [
       app: :joy,
-      version: "1.1.16",
+      version: "1.1.17",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
