2eebc019a92d4e6e5c82d3db950f40ab8afc805b
mc962 <11969409+mc962@users.noreply.github.com>
2026-03-30 12:33:02 -0400

    Improve channel node label

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Joy.MixProject do
   def project do
     [
       app: :joy,
-      version: "1.3.1",
+      version: "1.3.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
